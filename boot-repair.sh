7484b8ba8883f05d2b448af17e2aa88a3a4d07e5
Rômulo Cabral Santos <romulosantos11@hotmail.com>
2018-01-26 13:13:06 -0300

    rascunho, falta testar.
    falta testar o script, porém os comandos separados funciona muito bem

---
+++ boot-repair.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#created Rõmulo Santtos 26/01/2017
+
+su
+
+dnf install grub2 && dnf update grub2
+
+mkdir /mnt && mkdir /mnt/fedora
+mount /dev/mapper/fedora-root /mnt/fedora
+mount /dev/sda1 /mnt/fedora/boot
+
+cd /mnt/fedora
+mount -o bind /dev dev
+mount -o bind /proc proc
+mount -o bind /sys sys
+mount -t tmpfs tmpfs tmp
+
+$(chroot /mnt/fedora  | grub2-install /dev/sda | grub2-mkconfig -o /boot/grub2/grub.cfg)
+
